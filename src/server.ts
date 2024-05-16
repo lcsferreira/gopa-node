499166531df6cb3185226aa58383e1892ad00a86
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-05-16 13:54:22 -0300

    init: main structure

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
+import { routes } from "./routes";
+import { AppError } from "./errors/AppError";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal Server Error - ${err.message}`,
+    });
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
