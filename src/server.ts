4fbbee3dd3f6b7af54ed2ee5ac8e23c89f55e96e
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-10-02 15:27:53 -0300

    fix: solve cors localhost (test only)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import "express-async-errors";
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { routes } from "./routes";
 import { AppError } from "./errors/AppError";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
-import { authenticate } from "./middleware/authenticate";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    credentials: true,
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
@@ -16,6 +24,7 @@ app.use(routes);
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
+      console.log(err);
       return response.status(err.statusCode).json({
         status: "error",
         message: err.message,
@@ -28,6 +37,11 @@ app.use(
     });
   }
 );
+
+// Rota para a raiz "/"
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
