3d3b6b9e91180b6bdd7a550346661c922c559bd7
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-10-02 15:28:07 -0300

    feat: add forgot password route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { LoginController } from "../modules/auth/useCases/Login/LoginController";
+import { ForgotPasswordController } from "../modules/auth/useCases/ForgotPassword/ForgotPasswordController";
 
 const loginController = new LoginController();
+const forgotPasswordController = new ForgotPasswordController();
 
 const authRoutes = Router();
 
 authRoutes.post("/login", loginController.handle);
+authRoutes.post("/forgot-password", forgotPasswordController.handle);
 
 export { authRoutes };
